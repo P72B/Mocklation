a578e845ef38b2764046db4412379e84d2be5283
p72b <willy.riechert@carjump.de>
2018-01-20 14:20:39 +0100

    Fixed bug that notification can't be killed.

--- app/src/main/java/de/p72b/mocklation/notification/NotificationBroadcastReceiver.java
+++ app/src/main/java/de/p72b/mocklation/notification/NotificationBroadcastReceiver.java
@@ -1,11 +1,18 @@
 package de.p72b.mocklation.notification;
 
+import android.app.ActivityManager;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
+import de.p72b.mocklation.R;
+import de.p72b.mocklation.service.AppServices;
 import de.p72b.mocklation.service.location.MockLocationService;
+import de.p72b.mocklation.service.setting.Setting;
 import de.p72b.mocklation.util.AppUtil;
 
 
@@ -17,6 +24,12 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
         if (action == null) {
+            return;
+        }
+        if (!isServiceRunning(context, MockLocationService.class)) {
+            closeNotification(context);
+            ((Setting) AppServices.getService(AppServices.SETTINGS)).setMockLocationItemCode(null);
+            Toast.makeText(context, R.string.error_1018, Toast.LENGTH_LONG).show();
             return;
         }
         switch (action) {
@@ -33,5 +46,26 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver {
                 AppUtil.sendLocalBroadcast(context, new Intent(MockLocationService.EVENT_STOP));
                 break;
         }
+    }
+
+    private void closeNotification(Context context) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(
+                Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.cancel(MockLocationService.NOTIFICATION_ID);
+        }
+    }
+
+    private boolean isServiceRunning(Context context, Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        if (manager == null) {
+            return false;
+        }
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
