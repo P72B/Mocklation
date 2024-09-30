64fa71583c1502f6981a3bb4f57e2d782ba6e332
p72b <willy.riechert@googlemail.com>
2024-09-30 21:37:01 +0200

    Add empty list placeholder ghost in thron legacy color theme

--- app/src/main/kotlin/de/p72b/mocklation/ui/model/collection/CollectionPage.kt
+++ app/src/main/kotlin/de/p72b/mocklation/ui/model/collection/CollectionPage.kt
@@ -1,12 +1,15 @@
 package de.p72b.mocklation.ui.model.collection
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -30,7 +33,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
@@ -59,7 +64,7 @@ fun CollectionPage(
             )
         }
 
-        CollectionUIState.Empty -> EmptyCollectionScreen(modifier)
+        CollectionUIState.Empty -> EmptyListPlaceholder()
         CollectionUIState.Error -> TODO()
         is CollectionUIState.ShowSimulationCancelDialog -> {
             val data = (items as CollectionUIState.ShowSimulationCancelDialog)
@@ -89,11 +94,22 @@ internal fun LoadingCollectionScreen(
 }
 
 @Composable
-internal fun EmptyCollectionScreen(
-    modifier: Modifier = Modifier
+fun EmptyListPlaceholder(
+    message: String = stringResource(id = R.string.placeholder_empty_list),
+    icon: Int = R.drawable.ic_empty_list,
 ) {
-    Column(modifier) {
-        Text(text = "Empty TODO")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier.size(64.dp),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = message)
     }
 }
 
